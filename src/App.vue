65c05ad51c59001dc78a23eabf9071389c61da0b
Demetrio Turchetta <Kensir96@gmail.com>
2022-02-20 19:12:11 +0100

    Aggiustamenti e push post bonus

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <ilPrimissimo/>
+    <bannerHero/>
+  </div>
+</template>
+
+<script>
+import ilPrimissimo from './components/ilPrimissimo.vue'
+import bannerHero from './components/bannerHero.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ilPrimissimo,
+    bannerHero
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/bannerHero.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="jumbo-img">
+        <div class="jumbo-txt">
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+  name: 'bannerHero'
+}
+</script>
+
+
+<style scoped>
+   .jumbo-img {
+       margin: auto;
+       background-size: contain;
+       width: 80%;
+       background-repeat: no-repeat;
+       background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1920px-Marvel_Logo.svg.png');
+       background-position: center;
+       display: flex;
+       justify-content: center;
+       align-items: center;
+   }
+   .jumbo-txt {
+       margin: 80px;
+       color: #fff;
+       font-family: sans-serif;
+       font-size: 20px;
+       text-align: center;
+   }
+   .jumbo-txt h1 {
+       font-size: 100px;
+       margin-bottom: 10px;
+   }
+</style>
